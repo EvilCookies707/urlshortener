9fdacc221e9114f7c0c11928e7ebeb9de9ea6fd3
EvilCookies707 <91475156+EvilCookies707@users.noreply.github.com>
2023-09-21 18:50:23 +0000

    made get link function

--- netlify/functions/get-link.js
+++ netlify/functions/get-link.js
@@ -1,17 +1,45 @@
-import { withPlanetscale } from "@netlify/planetscale";
+import { MongoClient, ServerApiVersion } from 'mongodb'
 
-export const handler = withPlanetscale(async (event, context) => {
-  // const {
-  //   planetscale: { connection },
-  // } = context;
+export const handler = async (event, context) => {
+  const pathBlocks = event.path.split('/')
+  const id = pathBlocks[pathBlocks.length - 1]
 
-  // await connection.execute("INSERT INTO users (email, name) VALUES (?, ?)", [
-  //   email,
-  //   name,
-  // ]);
-  //poopy
+  if (!id) return { statusCode: 400 }
 
-  return {
-    statusCode: 200, body: event
-  };
-});
+  const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_TOKEN}@${process.env.MONGO_HOST}/?retryWrites=true&w=majority`
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true
+    }
+  })
+
+  await client.connect()
+
+  try {
+    const db = client.db('link-shortener')
+    const links = db.collection('links')
+    const query = { id }
+
+    const link = await links.findOne(query)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(link),
+      headers: {
+        "Content-Type": 'application/json'
+      }
+    }
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Unable to process link creation, please try again.', details: err}),
+      headers: {
+        "Content-Type": 'application/json'
+      }
+    }
+  } finally {
+    await client.close()
+  }
+}
